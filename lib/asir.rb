b014c900d4b902f8fd10f970f14480e1614e44fe
Kurt Stephens <ks.github@kurtstephens.com>
2010-12-10 16:41:31 -0600

    Refactored ASIR::Client.

--- lib/asir.rb
+++ lib/asir.rb
@@ -754,53 +754,10 @@ module ASIR
     # !SLIDE resume
   end
   # !SLIDE END
-
-
-  # !SLIDE
-  # Mixin Client support to any Module
-  #
-  # Extend Module with #client proxy support.
-  module Client
-    def self.included target
-      super
-      target.extend ModuleMethods if Module === target
-    end
-    
-    module ModuleMethods
-      def client
-        @client ||=
-          ASIR::Client::Proxy.new(:receiver => self)
-      end
-    end
-
-    # !SLIDE
-    # Client Proxy
-    #
-    # Provide client interface proxy to a service.
-    class Proxy
-      include Log, Initialization
-      
-      attr_accessor :receiver, :transport
-      
-      def transport
-        @transport ||=
-          Transport::Local.new
-      end
-      
-      # Accept all other messages to be encoded and transported to a service.
-      def method_missing selector, *arguments
-        raise ArgumentError, "block given" if block_given?
-        _log { "method_missing #{selector.inspect} #{arguments.inspect}" }
-        request = Request.new(receiver, selector, arguments)
-        result = transport.send_request(request)
-        result
-      end
-    end
-    # !SLIDE END
-  end
-  # !SLIDE END
 end
 # !SLIDE END
+
+require 'asir/client'
 
 # !SLIDE
 # Synopsis
