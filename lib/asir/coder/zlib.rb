2c316061ca7b5f21b258ba28ac8b3406d58baf4d
Kurt Stephens <ks.github@kurtstephens.com>
2011-05-18 19:28:54 -0500

    Added Zlib coder.

---
+++ lib/asir/coder/zlib.rb
@@ -0,0 +1,21 @@
+require 'asir'
+
+require 'zlib'
+
+module ASIR
+  class Coder
+    class Zlib < self
+      attr_accessor :compression_level
+
+      def _encode obj
+        raise TypeError unless String === obj
+        ::Zlib::Deflate.deflate(obj, @compression_level || ::Zlib::DEFAULT_COMPRESSION)
+      end
+      def _decode obj
+        raise TypeError unless String === obj
+        ::Zlib::Inflate.inflate(obj)
+      end
+    end
+  end
+end
+
