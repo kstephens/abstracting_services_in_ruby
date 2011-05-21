f7898ae7292f8b73f4a990f3234a6a77728bfda6
Kurt Stephens <ks.github@kurtstephens.com>
2011-05-20 20:15:18 -0500

    Improved formatting of SPEC_VERBOSE option.

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -21,12 +21,12 @@ describe "ASIR Example" do
       end
     end
 
-    $stderr.puts "running #{cmd}" if ENV['SPEC_VERBOSE']
+    $stderr.puts "\n   Running #{cmd}:" if ENV['SPEC_VERBOSE']
     @output = `#{cmd} 2>&1`
     $stderr.write @output  if ENV['SPEC_VERBOSE']
     @expects.empty?.should_not == true
     @expects.each do | rx, mode |
-      $stderr.puts "  checking #{mode} #{rx.inspect}" if ENV['SPEC_VERBOSE']
+      $stderr.puts "    Checking #{mode} #{rx.inspect}" if ENV['SPEC_VERBOSE']
       case mode
       when :'=~'
         @output.should =~ rx
