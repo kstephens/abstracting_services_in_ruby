c365ef083e3405b2aaf6c14041215802a885c259
Kurt Stephens <kurt@enovafinancial.com>
2012-01-19 16:54:23 -0600

    Improved Coder::XML. Support for generated DOM :id attributes. Support multiple references using :idref attributes. Support shorter String, Symbol and Numeric attribute encodings. Some performance and security enhancements.

--- lib/asir/coder/xml.rb
+++ lib/asir/coder/xml.rb
@@ -1,33 +1,54 @@
 require 'asir'
+require 'asir/object_resolving'
 
 gem 'libxml-ruby'
 require 'xml'
 
 module ASIR
   class Coder
+    # !SLIDE
+    # XML
+    #
+    # Encode/Decode objects as XML.
     class XML < self
+      class Error < ::ASIR::Error
+        class BadIdref < self; end
+      end
       def _encode obj
         @stream = ''
-        @dom_id_map = { } # not used, yet
-        @dom_id = 0 # not used, yet
+        @dom_id_map = { }
+        @dom_id = 0
+        @cls_tag_map = { }
         _encode_dom obj
         @stream
       end
 
       def _decode obj
         @stream = obj
-        @dom_id_map = { } # not used, yet
-        @dom_id = 0 # not used, yet
+        @decoder ||= DECODER
+        @dom_id_map = { }
+        @dom_id = 0
+        @cls_tag_map = { }
         @parser = ::XML::Parser.string(@stream)
         @dom = @parser.parse
-        _decode_dom @dom.root
+        decode_dom @dom.root
+      end
+
+      def encode_dom obj
+        if dom_id = @dom_id_map[obj.object_id]
+          tag_obj(obj, nil, :idref => dom_id.first)
+        else
+          _encode_dom obj
+        end
       end
 
       def _encode_dom obj
         case obj
         when NilClass, TrueClass, FalseClass
           tag_obj(obj)
-        when Symbol, Numeric
+        when Numeric
+          tag_obj(obj, nil, :v => obj.to_s)
+        when Symbol
           tag_obj(obj) do
             @stream << obj.to_s
           end
@@ -38,14 +59,14 @@ module ASIR
         when Array
           tag_obj(obj, :id) do 
             obj.each do | elem |
-              _encode_dom elem
+              encode_dom elem
             end
           end
         when Hash
           tag_obj(obj, :id) do 
             obj.each do | key, val |
-              _encode_dom key
-              _encode_dom val
+              encode_dom key
+              encode_dom val
             end
           end
         else
@@ -54,92 +75,138 @@ module ASIR
               val = obj.instance_variable_get(attr)
               key = attr.to_s.sub(/^@/, '')
               tag(key) do 
-                _encode_dom val
+                encode_dom val
               end
             end
           end
         end
       end
 
-      def tag_obj obj, with_id = false
-        tag_name = obj.class.name.gsub('::', '.')
+      def tag_obj obj, with_id = false, attrs = nil
         if block_given?
-          tag(tag_name, with_id ? { :id => obj.object_id } : nil) do
+          tag(cls_tag(obj), with_id ? { with_id => map_obj_to_dom_id!(obj) } : nil) do
             yield
           end
         else
-          tag tag_name
+          tag(cls_tag(obj), attrs)
         end
       end
 
+      CC = '::'.freeze; D = '.'.freeze
+      def cls_tag obj
+        obj = obj.class
+        @cls_tag_map[obj] ||= obj.name.gsub(CC, D).freeze
+      end
+
+      def map_obj_to_dom_id! obj
+        if dom_id = @dom_id_map[obj.object_id]
+          dom_id.first
+        else
+          @dom_id_map[obj.object_id] = [ @dom_id += 1, obj ]
+          @dom_id
+        end
+      end
+
+      B  = '<'.freeze;  S = ' '.freeze; E = '>'.freeze; SE = '/>'.freeze
+      BS = '</'.freeze; A = '='.freeze
       def tag tag, attrs = nil
         tag = tag.to_s
-        if block_given?
-          @stream << '<' << tag << ' '
-          if attrs
-            attrs.each do | key, val |
-              @stream << key.to_s << '=' << val.to_s.inspect << ' '
-            end
+        @stream << B << tag << S
+        if attrs
+          attrs.each do | key, val |
+            @stream << key.to_s << A << val.to_s.inspect << S
           end
-          @stream << '>'
-          yield
-          @stream << '</' << tag << '>'
+        end
+        if block_given?
+          @stream << E; yield; @stream << BS << tag << E
         else
-          @stream << '<' << tag << ' />'
+          @stream << SE
         end
       end
 
       ################################################################
 
+      def decode_dom dom
+        if dom_id = dom.attributes[:idref]
+          unless obj = @dom_id_map[dom_id]
+            raise Error::BadIdref, "in element #{dom}"
+          end
+          obj
+        else
+          obj = _decode_dom(dom)
+          map_dom_id_to_obj! dom, obj if dom.attributes[:id]
+          obj
+        end
+      end
+
       def _decode_dom dom
         cls_name = dom.name
-        case cls_name
-        when "String"
-          dom.content
-        when "Symbol"
-          dom.content.to_sym
-        when "Fixnum", "Bignum"
-          dom.content.to_i
-        when "Float"
-          dom.content.to_f
-        when "NilClass"
-          nil
-        when "TrueClass"
-          true
-        when "FalseClass"
-          false
-        when "Hash"
+        decoder = @decoder[cls_name] || @decoder['Object']
+        raise Error, "BUG: " unless decoder
+        decoder.call(self, dom)
+      end
+
+      DECODER = {
+        'NilClass'   => lambda { | _, dom | nil },
+        'TrueClass'  => lambda { | _, dom | true },
+        'FalseClass' => lambda { | _, dom | false },
+        'String'     => lambda { | _, dom | (dom.attributes[:v] || dom.content) },
+        'Symbol'     => lambda { | _, dom | (dom.attributes[:v] || dom.content).to_sym },
+        'Integer'    => lambda { | _, dom | (dom.attributes[:v] || dom.content).to_i },
+        'Float'      => lambda { | _, dom | (dom.attributes[:v] || dom.content).to_f },
+        "Array" => lambda { | _, dom | 
+          obj = [ ]
+          _.map_dom_id_to_obj! dom, obj
+          dom.each_element do | elem |
+            obj << _.decode_dom(elem)
+          end
+          obj
+        },
+        'Hash' => lambda { | _, dom |
           obj = { }
+          _.map_dom_id_to_obj! dom, obj
           key = nil
           dom.each_element do | val |
             if key
-              obj[_decode_dom(key)] = _decode_dom(val)
+              obj[_.decode_dom(key)] = _.decode_dom(val)
               key = nil
             else
               key = val
             end
           end
           obj
-        when "Array"
-          obj = [ ]
-          dom.each_element do | elem |
-            obj << _decode_dom(elem)
-          end
-          obj
-        else
+        },
+        'Object' => lambda { | _, dom |
+          cls_name = dom.name
           # $stderr.puts "cls_name = #{cls_name.inspect}"
-          cls_name = cls_name.gsub('.', '::')
-          # $stderr.puts "cls_name = #{cls_name.inspect}"
-          cls = eval("::#{cls_name}") # NASTY!
+          cls = _.tag_cls(cls_name)
           obj = cls.allocate
+          _.map_dom_id_to_obj! dom, obj
           dom.each_element do | child |
             key = child.name
-            val = _decode_dom child.first
+            val = _.decode_dom child.first
             obj.instance_variable_set("@#{key}", val)
           end
           obj
+        },
+      }
+      DECODER['Fixnum'] = DECODER['Bignum'] = DECODER['Integer']
+
+      def map_dom_id_to_obj! dom, obj
+        dom_id = dom.attributes[:id]
+        debugger unless dom_id
+        raise Error, "no :id attribute in #{dom}" unless dom_id
+        if (other_obj = @dom_id_map[dom_id]) and other_obj.object_id != obj.object_id
+          raise Error, "BUG: :id #{dom_id} already used for #{other_obj.class.name} #{other_obj.inspect}"
         end
+        @dom_id_map[dom_id] = obj
       end
+
+      include ObjectResolving
+      def tag_cls cls_name
+        @cls_tag_map[cls_name.freeze] ||= resolve_object(cls_name.gsub('.', '::'))
+      end
+
     end
   end
 end
--- spec/xml_spec.rb
+++ spec/xml_spec.rb
@@ -26,6 +26,16 @@ describe "ASIR::Coder::XML" do
   [
    1234,
    1.234,
+  ].each do | x |
+    basic_objs << x
+    it "should handle #{x.inspect}" do
+      xml = @enc.encode(x)
+      xml.should == "<#{x.class.name} v=\"#{x.to_s}\" />"
+      @dec.decode(xml).should == x
+    end
+  end
+
+  [
    :symbol,
   ].each do | x |
     basic_objs << x
@@ -42,7 +52,7 @@ describe "ASIR::Coder::XML" do
     basic_objs << x
     it "should handle #{x.inspect}" do
       xml = @enc.encode(x)
-      xml.should == "<#{x.class.name} id=\"#{x.object_id}\" >#{x.to_s}</#{x.class.name}>"
+      xml.should == "<#{x.class.name} id=\"1\" >#{x.to_s}</#{x.class.name}>"
       @dec.decode(xml).should == x
     end
   end
@@ -50,32 +60,33 @@ describe "ASIR::Coder::XML" do
   it "should handle empty Array" do
     x = [ ]
     xml = @enc.encode(x)
-    xml.should == "<#{x.class.name} id=\"#{x.object_id}\" ></#{x.class.name}>"
+    xml.should == "<#{x.class.name} id=\"1\" ></#{x.class.name}>"
     @dec.decode(xml).should == x
   end
 
   it "should handle Array" do
     x = [ *basic_objs ]
     xml = @enc.encode(x)
-    xml.should =~ %r{\A<#{x.class.name} id=\"#{x.object_id}\" ><NilClass /><TrueClass /><FalseClass /><Fixnum >1234</Fixnum><Float >1.234</Float><Symbol >symbol</Symbol><String id=\"[^"]+\" >String</String></#{x.class.name}>\Z} # " emacs
+    xml.should =~ %r{\A<#{x.class.name} id=\"1\" ><NilClass /><TrueClass /><FalseClass /><Fixnum v="1234" /><Float v="1.234" /><Symbol >symbol</Symbol><String id=\"[^"]+\" >String</String></#{x.class.name}>\Z} # " emacs
     @dec.decode(xml).should == x
   end
 
   it "should handle empty Hash" do
     x = { }
     xml = @enc.encode(x)
-    xml.should == "<#{x.class.name} id=\"#{x.object_id}\" ></#{x.class.name}>"
+    xml.should == "<#{x.class.name} id=\"1\" ></#{x.class.name}>"
     @dec.decode(xml).should == x
   end
 
   it "should handle Hash" do
     x = Hash[ *basic_objs.map{|e| e.inspect}.zip(basic_objs).flatten ]
     xml = @enc.encode(x)
-    xml.should =~ %r{<#{x.class.name} id=\"#{x.object_id}\" >}
-    xml.should =~ %r{</#{x.class.name}>}
+    xml.should =~ %r{\A<#{x.class.name} id=\"1\" >}
+    xml.should =~ %r{</#{x.class.name}>\Z}
     basic_objs.each do | v |
       vx = @enc.dup.encode(v)
-      xml.should =~ %r{#{vx}}
+      vx = vx.gsub(/id="[^"]+"/, 'id="\d+"')
+      xml.should =~ Regexp.new(vx)
       xml.should =~ %r{ >#{v.inspect}</String>}
     end
     @dec.decode(xml).should == x
@@ -92,7 +103,7 @@ describe "ASIR::Coder::XML" do
     x.o = ASIR::Coder::XML::Test.new
     x.o.a = 123
     xml = @enc.encode(x)
-    xml.should =~ %r{<#{x.class.name.gsub('::', '.')} id=\"#{x.object_id}\" >}
+    xml.should =~ %r{<#{x.class.name.gsub('::', '.')} id=\"1\" >}
     xml.should =~ %r{</#{x.class.name.gsub('::', '.')}>}
     y = @dec.decode(xml)
     y.a.should == x.a
@@ -102,4 +113,33 @@ describe "ASIR::Coder::XML" do
     x.instance_variables.sort { |a, b| a.to_s <=> b.to_s}.should == 
       y.instance_variables.sort { | a, b | a.to_s <=> b.to_s }
   end
+
+  it "should handle multiple references to same objects." do
+    x = Hash[ *basic_objs.map{|e| e.inspect}.zip(basic_objs).flatten ]
+    y = [ 1, 2 ]
+    x = [ x, x, y, y ]
+    xml = @enc.encode(x)
+    y = @dec.decode(xml)
+    y[0].object_id.should == y[1].object_id
+    y[2].object_id.should == y[3].object_id
+  end
+
+  it "should handle self-referencing Array." do
+    x = [ 1 ]
+    x << x
+    xml = @enc.encode(x)
+    y = @dec.decode(xml)
+    y[0].should == x[0]
+    y[1].object_id.should == y.object_id
+  end
+
+  it "should handle self-referencing Hash." do
+    x = { :a => 1 }
+    x[:self] = x
+    xml = @enc.encode(x)
+    y = @dec.decode(xml)
+    y[:a].should == x[:a]
+    y[:self].object_id.should == y.object_id
+  end
+
 end
