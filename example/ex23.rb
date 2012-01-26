c26939fe9b86526adf200c26755f40a7d6b13ab8
Kurt Stephens <ks.github@kurtstephens.com>
2012-01-25 20:22:30 -0600

    Working Subprocess service with continuation.

---
+++ example/ex23.rb
@@ -0,0 +1,21 @@
+#
+# !SLIDE :capture_code_output true
+# Subprocess service with continuation
+
+require 'example_helper'
+begin
+  Email.client.transport = t =
+    ASIR::Transport::Subprocess.new(:one_way => true)
+
+  pr(Email.client.send_email(:pdf_invoice,
+                             :to => "user@email.com",
+                             :customer => @customer) { | resp |
+     pr [ :in_block, resp.result ] })
+end
+
+# !SLIDE END
+# EXPECT: : client process
+# EXPECT: : Email.send_mail :pdf_invoice
+# EXPECT: : pr: nil
+# EXPECT: : pr: [ :in_block, :ok ]
+
--- lib/asir/transport/subprocess.rb
+++ lib/asir/transport/subprocess.rb
@@ -7,16 +7,22 @@ module ASIR
     #
     # Send one-way Request to a forked subprocess.
     class Subprocess < Local
+      def initialize *args
+        @one_way = true; super
+      end
+
       def _send_request request, request_payload
-        Process.fork do 
-          super
+        Process.fork do
+          send_response(super, nil, nil)
         end
-        nil # opaque_response
       end
 
       # one-way; no Response
       def _receive_response request, opaque_response
-        nil
+      end
+
+      # one-way; no Response
+      def _send_response request, response, response_payload, stream, request_state
       end
     end
     # !SLIDE END
