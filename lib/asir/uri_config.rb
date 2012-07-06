f1ad002e3ea14e475578532db34cb9b43f9f0a75
Kurt Stephens <kurt@enova.com>
2012-07-06 09:06:38 -0500

    Support for scheme, host and port defaults.

--- lib/asir/uri_config.rb
+++ lib/asir/uri_config.rb
@@ -4,6 +4,7 @@ require 'uri'
 module ASIR
   module UriConfig
     attr_accessor :uri, :scheme, :host, :port
+    attr_accessor :scheme_default, :host_default, :port_default
     alias :protocol :scheme
     alias :protocol= :scheme=
     alias :address :host
@@ -20,20 +21,25 @@ module ASIR
 
     def scheme
       @scheme ||=
-        @uri ? _uri.scheme : S_TCP
+        (@uri && _uri.scheme) ||
+        @scheme_default ||
+        S_TCP
     end
     S_TCP = 'tcp'.freeze
 
     def host
       @host ||=
-        @uri ? _uri.host : S_LOCALHOST
+        (@uri && _uri.host) ||
+        @host_default ||
+        S_LOCALHOST
     end
     S_LOCALHOST = '127.0.0.1'.freeze
 
     def port
       @port ||=
-        @uri ? _uri.port :
-          (raise Error, "#{self.class}: port not set.")
+        (@uri && _uri.port) ||
+        @port_default ||
+        (raise Error, "#{self.class}: port not set.")
     end
 
   end
