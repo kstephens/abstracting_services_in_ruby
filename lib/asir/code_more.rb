642e55bb66e41ceeea43c30bc8fa6b3995a31793
Kurt Stephens <kurt@enova.com>
2012-11-30 11:15:58 -0600

    CodeMore: Use @receiver_name to disambiguate Models vs. Instances.

--- lib/asir/code_more.rb
+++ lib/asir/code_more.rb
@@ -7,11 +7,16 @@ module ASIR
     include ObjectResolving # resolve_object()
     include CodeBlock # encode_block!, decode_block!
 
+    attr_accessor :receiver_name
+
     def encode_more!
       obj = encode_block! # may self.dup
       unless ::String === @receiver_class
         obj ||= self.dup # dont dup twice.
-        obj.receiver = @receiver.name if ::Module === @receiver
+        if ::Module === @receiver
+          obj.receiver_name = true
+          obj.receiver = @receiver.name
+        end
         obj.receiver_class = @receiver_class.name
         if resp = obj.result and resp.message == self
           resp.message = obj
@@ -23,8 +28,9 @@ module ASIR
     def decode_more!
       decode_block!
       if ::String === @receiver_class
+        # pp [ :decode_more!, self ]
         @receiver_class = resolve_object(@receiver_class)
-        @receiver = resolve_object(@receiver) if ::Module === @receiver_class
+        @receiver = resolve_object(@receiver) if @receiver_name
         unless @receiver_class === @receiver
           raise Error, "receiver #{@receiver.class.name} is not a #{@receiver_class}" 
         end
