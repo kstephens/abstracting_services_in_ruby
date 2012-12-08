ff84a922747f733c060fa8122b2aa63bacf4466d
Kurt Stephens <ks.github@kurtstephens.com>
2012-12-07 20:48:50 -0600

    Handle :conduit_options and conduit pid files.

--- lib/asir/transport/conduit.rb
+++ lib/asir/transport/conduit.rb
@@ -4,9 +4,11 @@ module Asir
   class Transport
     # Conduit service support.
     module Conduit
+      attr_accessor :conduit_options, :conduit_pid
       def start_conduit! options = nil
         opts = { :fork => true }
         opts.update(options) if options
+        @conduit_options = opts
         _log { "start_conduit! #{self}" } if @verbose >= 1
         in_fork = opts[:fork]
         raise "already running #{@conduit_pid} #{@conduit_cmd}" if @conduit_pid
@@ -17,18 +19,28 @@ module Asir
             raise "Could not exec"
           end
           _log { "start_conduit! #{self} started pid=#{@conduit_pid.inspect}" } if @verbose >= 2
+          if pid_file = @conduit_options[:pid_file]
+            File.open(pid_file, "w") { | fh | fh.puts @conduit_pid }
+          end
         else
           _start_conduit!
         end
         self
       end
 
+      def conduit_pid
+        if ! @conduit_pid and pid_file = @conduit_options[:pid_file]
+          @conduit_pid = (File.read(pid_file).to_i rescue nil)
+        end
+        @conduit_pid
+      end
+
       def stop_conduit! opts = nil
-        if @conduit_pid
+        if conduit_pid
           _log { "stop_conduit! #{self} pid=#{@conduit_pid.inspect}" } if @verbose >= 1
           ::Process.kill( (opts && opts[:signal]) || 'TERM', @conduit_pid)
+          ::File.unlink(pid_file) rescue nil if pid_file
           ::Process.waitpid @conduit_pid
-          # File.unlink @redis_conf
         end
         self
       ensure
