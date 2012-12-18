f8666e2dbf8430ec67069be2cfcae89fe7891baa
Kurt Stephens <ks.github@kurtstephens.com>
2012-12-17 20:27:25 -0600

    Join spawned thread.

--- example/ex26.rb
+++ example/ex26.rb
@@ -6,14 +6,17 @@ require 'example_helper'
 begin
   Email.asir.transport = t =
     ASIR::Transport::Thread.new
-
+  spawned_thread = nil
   t.after_thread_new = lambda do | transport, message, thread |
+    spawned_thread = thread
     $stderr.puts "\n  #{$$}: Spawned Thread #{thread.inspect}"
   end
 
   pr Email.asir.send_email(:pdf_invoice,
                              :to => "user@email.com",
                              :customer => @customer)
+
+  spawned_thread.join
 end
 
 # !SLIDE END
