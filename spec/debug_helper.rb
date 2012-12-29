27a2414c49421e8e30c918917fcc370caaf84586
Kurt Stephens <ks.github@kurtstephens.com>
2012-12-29 02:37:38 -0600

    Remove ruby-debug dependency.

--- spec/debug_helper.rb
+++ spec/debug_helper.rb
@@ -4,17 +4,6 @@ when /java/
   gem 'json_pure'
 else
   gem 'json'
-  case RUBY_VERSION
-  when /^2\./
-    gem 'simplecov'
-  when /^1\.9/
-    gem 'ruby-debug19'
-    gem 'simplecov'
-    # require 'ruby-debug' # BROKEN in 1.9.3-head
-  when /^1\.8/
-    gem 'ruby-debug'
-    gem 'simplecov'
-    require 'ruby-debug'
-  end
+  gem 'simplecov'
 end
 
