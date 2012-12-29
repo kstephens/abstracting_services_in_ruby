163bab2d2c79dc2b6539adf66674cfa2b33c4d07
Kurt Stephens <ks.github@kurtstephens.com>
2012-12-29 01:43:31 -0600

    Remove deprecated #client, #server attributes.

--- lib/asir/identity.rb
+++ lib/asir/identity.rb
@@ -7,12 +7,6 @@ module ASIR
   module Identity
     attr_accessor :identifier, :timestamp
 
-    # Optional: Opaque data about the Client that created the Message.
-    attr_accessor :client
-
-    # Optional: Opaque data about the Service that handled the Result.
-    attr_accessor :server
-
     # Creates a thread-safe unique identifier.
     def create_identifier!
       @identifier ||= ::ASIR::UUID.counter_uuid
