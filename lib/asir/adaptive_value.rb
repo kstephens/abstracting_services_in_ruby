5c846e8ee4ac610eaa5de0c4934de4e31a36818f
Kurt Stephens <ks.github@kurtstephens.com>
2013-06-19 16:01:14 -0500

    Added ASIR::AdaptiveValue for use in RetryBehavior and worker graceful restart.

---
+++ lib/asir/adaptive_value.rb
@@ -0,0 +1,69 @@
+module ASIR
+  # Adaptive Value.
+  # Return a Numeric #value which adapts.
+  # Useful for controlling adaptive retry sleep amounts or other
+  # values that must be stochastic.
+  #
+  # #init must be specified.
+  # #rand_factor should be a Float.
+  #
+  class AdaptiveValue
+    attr_accessor :init, :min, :max, :add, :mult, :rand_factor
+
+    def initialize opts = nil
+      if opts
+        opts.each do | k, v |
+          send(:"#{k}=", v)
+        end
+      end
+    end
+
+    # Returns a new value limited by #min and #max after applying the addition of #rand_factor.
+    def value
+      v = @value || init_or_error
+      v += v * rand(@rand_factor) if @rand_factor
+      v = @min if @min && v < @min
+      v = @max if @max && v > @max
+      v
+    end
+    def value= x
+      @value = x
+    end
+
+    # Returns a cached #value until #reset! or #new_value!.
+    def value!
+      @value_ ||= value
+    end
+
+    # Returns a new cached #value.
+    def new_value!
+      @value_ = nil
+      value!
+    end
+
+    # Resets #value to #init.
+    def reset!
+      @value_ = @value = nil
+      self
+    end
+
+    # Increments value by #add, if #add is set.
+    # Multiplies value by #mult, if #mult is set.
+    # Limits value by #min and #max.
+    def adapt!
+      @value ||= init_or_error
+      @value += @add if @add
+      @value *= @mult if @mult
+      @value = @min if @min && @value < @min
+      @value = @max if @max && @value > @max
+      self
+    end
+
+    private
+    def init_or_error
+      @init or raise ArgumentError, "init: not set"
+    end
+
+  end
+end
+
---
+++ spec/adaptive_value_spec.rb
@@ -0,0 +1,94 @@
+require 'asir/adaptive_value'
+
+describe ASIR::AdaptiveValue do
+  it "should have a #value of #init." do
+    av = ASIR::AdaptiveValue.new(:init => 5)
+    av.value.should == 5
+    av.value.should == 5
+    av.value!.should == 5
+  end
+
+  it "should have a #value limited by #min." do
+    av = ASIR::AdaptiveValue.new(:init => 5, :min => 10)
+    av.value.should == 10
+    av.value.should == 10
+    av.value!.should == 10
+  end
+
+  it "should have a #value not limited by #min." do
+    av = ASIR::AdaptiveValue.new(:init => 15, :min => 10)
+    av.value.should == 15
+    av.value.should == 15
+    av.value!.should == 15
+  end
+
+  it "should have a #value limited by #max." do
+    av = ASIR::AdaptiveValue.new(:init => 5, :max => 10)
+    av.value.should == 5
+    av.value.should == 5
+    av.value!.should == 5
+  end
+
+  it "should have a #value not limited by #max." do
+    av = ASIR::AdaptiveValue.new(:init => 15, :max => 10)
+    av.value.should == 10
+    av.value.should == 10
+    av.value!.should == 10
+  end
+
+  it "should have a #value adjusted by #add." do
+    av = ASIR::AdaptiveValue.new(:init => 1, :add => 2)
+    av.value.should == 1
+    av.value.should == 1
+    av.adapt!
+    av.value.should == 3
+    av.value.should == 3
+    av.value!.should == 3
+    av.adapt!
+    av.value.should == 5
+    av.value.should == 5
+    av.value!.should == 3
+  end
+
+  it "should have a #value adjusted by #mult." do
+    av = ASIR::AdaptiveValue.new(:init => 10, :mult => 1.2)
+    av.value.should == 10
+    av.value.should == 10
+    av.adapt!
+    av.value.to_i.should == 12
+    av.value.to_i.should == 12
+    av.value!.to_i.should == 12
+    av.adapt!
+    av.value.to_i.should == 14
+    av.value.to_i.should == 14
+    av.value!.to_i.should == 12
+  end
+
+  it "should have a #value adjusted by #rand_factor." do
+    av = ASIR::AdaptiveValue.new(:init => 10, :rand_factor => 0.5)
+    def av._ri
+      @_ri ||= -1
+      @_ri += 1
+    end
+    def av._rv
+      @_rv ||= (0...10).to_a
+    end
+    def av.rand scale
+      (_rv[_ri % _rv.size] / 10.0) * scale
+    end
+
+    av.value.should == 10
+    av.value.should == 10.5
+    av.value!.should == 11.0
+    av.value.should == 11.5
+    av.value.should == 12.0
+    av.value!.should == 11.0
+  end
+
+  it "should raise error if #init is not set." do
+    av = ASIR::AdaptiveValue.new(:rand_factor => 0.5)
+    lambda do
+      av.value
+    end.should raise_error(ArgumentError, /init: not set/)
+  end
+end
