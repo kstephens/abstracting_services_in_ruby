0184adce5f08142f6fbd998d60f4b9f0d185e626
Kurt Stephens <ks.github@kurtstephens.com>
2013-09-26 21:28:31 -0500

    Guard support.

--- asir.gemspec
+++ asir.gemspec
@@ -27,10 +27,12 @@ Gem::Specification.new do |s|
   s.add_dependency "rack", "~> 1.4.1"
 
   s.add_development_dependency 'rake', '>= 0.9.0'
-  s.add_development_dependency 'rspec', '~> 2.12.0'
+  s.add_development_dependency 'rspec', '~> 2.13.0'
   s.add_development_dependency 'simplecov', '>= 0.1'
   s.add_development_dependency 'minitest', '>= 5.0'
   s.add_development_dependency 'mocha', '~> 0.14'
+  s.add_development_dependency 'guard', '~> 1.8'
+  s.add_development_dependency 'guard-rspec', '~> 3.1'
   if (RUBY_ENGINE rescue 'UNKNOWN') =~ /jruby/i
     s.add_development_dependency 'spoon', '>= 0.0.1'
   end
---
+++ spec/lib/asir/main_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+require 'asir/main'
+
+describe ASIR::Main do
+  let(:main) { ASIR::Main.new }
+
+  context "#server_pid" do
+    let(:pid_file) do
+      pid_file = "/tmp/#{$$}.#{rand(10000)}.pid"
+      File.exist?(pid_file).should == false
+      main.stubs(:pid_file).returns pid_file
+      pid_file
+    end
+    after(:each) { File.unlink(pid_file) rescue nil }
+
+    it "should return non-true if pid_file does not exist" do
+      main.server_pid.should == false
+    end
+
+    it "should return non-true if pid_file is empty" do
+      File.open(pid_file, "w+") { | fh | }
+      main.server_pid.should == nil
+    end
+
+    it "should return non-true if pid_file is empty" do
+      File.open(pid_file, "w+") { | fh | }
+      main.server_pid.should == nil
+    end
+
+    it "should return non-true if pid_file has an empty line" do
+      File.open(pid_file, "w+") { | fh | fh.puts "" }
+      main.server_pid.should == false
+    end
+
+    it "should return non-true if pid_file is broken" do
+      File.open(pid_file, "w+") { | fh | "asdfsd" }
+      main.server_pid.should == nil
+    end
+
+    it "should return pid if pid_file contains a pid" do
+      File.open(pid_file, "w+") { | fh | fh.puts($$.to_s) }
+      main.server_pid.should == $$
+    end
+  end
+
+  context "#process_running?" do
+    it "should return false for invalid values." do
+      main.process_running?(nil).should == nil
+      main.process_running?(false).should == false
+      main.process_running?(0).should == false
+      main.process_running?(1).should == false
+    end
+
+    it "should return pid for a live process." do
+      main.process_running?($$).should == $$
+    end
+  end
+end
